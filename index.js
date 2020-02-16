69090a8b95ef388d9d16d15607e7e33dcb1796d4
mafukuda171080 <mafukuda171080@gmail.com>
2020-02-16 22:30:23 +0900

    2 files modified

--- index.js
+++ index.js
@@ -25,19 +25,3 @@ class App extends Component {
     );
   }
 }
-
-function addList(obj) {
-  // tbody要素に指定したIDを取得し、変数「tbody」に代入
-  var tbody = document.getElementById('p2146-tbody');
-  // objの親の親のノードを取得し（つまりtr要素）、変数「tr」に代入
-  var tr = obj.parentNode.parentNode;
-  // tbodyタグ直下のノード（行）を複製し、変数「list」に代入
-  var list = tbody.childNodes[0].cloneNode(true);
-  // 複製した行の2番目のセルを指定し、変数「td」に代入
-  var td = list.childNodes[1];
-  // 複製した行の2番目のセルの内容を「A」に置き換え
-  td.textContent = 'A';
-  //　複製したノード「list」を直後の兄弟ノードの上に挿入
-  // （「tr」の下に挿入）
-  tbody.insertBefore(list, tr.nextSibling);
-}
